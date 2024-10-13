f33b06068334a9396bf23363c50406e17c07ad24
uzz-ptl7 <uzair07patel@gmail.com>
2024-10-13 18:34:03 +0200

    Complete Add, Delete, Update Functions

--- script.js
+++ script.js
@@ -1,47 +1,98 @@
-let tasks = ["Buy milk", "Clean the room", "Go to the gym"];
+// ....................................................Array of Tasks............................................
+let tasks = ["Buy milk","Clean the room","Go to the gym"];
 
+// ........................Display Tasks, Create Tags and Buttons for Updating and Deleting Tasks..............................
 const displayTasks = () => {
-    let taskDisplay = document.querySelector("#taskDisplay");
-    let container = document.querySelector(".container");
-    taskDisplay.innerHTML = "";
+    let taskDisplay = document.querySelector('#taskDisplay');
+    taskDisplay.innerHTML = '';
+
     tasks.forEach((task, index) => {
-        const taskItem = document.createElement('li');
-        // container.classList.add('bg-red-600')
-        taskItem.classList.add('bg-blue-200', 'p-2', 'm-2', 'rounded-lg');
-        taskItem.textContent = task;
-        taskDisplay.appendChild(taskItem);
+    const taskItem = document.createElement('li');
+    taskItem.classList.add('bg-black', 'text-gray-200', 'p-2', 'rounded-lg','flex','justify-between', 'w-[60%]', 'm-auto', 'my-2');
+    
+    const taskText = document.createElement('span');
+
+    taskText.textContent = task;
+    taskItem.appendChild(taskText);
+
+    const taskLinks = document.createElement('div');
+    taskLinks.classList.add('task-links');
+
+    // ..................................................Task Update Button......................................
+    const updateButton = document.createElement('a');
+    updateButton.href = '#';
+    updateButton.textContent = '✏️';
+    updateButton.classList.add('mr-4');
+    updateButton.addEventListener('click', () => editTask(index));
+    taskLinks.appendChild(updateButton);
+
+    // ..................................................Task Delete Button......................................
+    const deleteButton = document.createElement('a');
+    deleteButton.href = '#';
+    deleteButton.textContent = '🗑️';
+    // deleteButton.classList.add('text-black');
+    deleteButton.addEventListener('click', () => deleteTask(index));
+    taskLinks.appendChild(deleteButton);
+    
+ 
+    taskItem.appendChild(taskLinks);
+
+    // taskItem.textContent = task;
+    taskDisplay.appendChild(taskItem);
     })
 }
 
-const saveTasksToLocalStorage = () => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+//...............................Functions..........................................  
+
+// ..................................................Save Task to Local Storage......................................
+const saveTaskToLocalStorage = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
+// ..................................................Add Task to the Array......................................
 const addTask = () => {
-    const newTaskInput = document.querySelector("#newTask");
+    const newTaskInput = document.querySelector('#newTask');
     const newTask = newTaskInput.value;
 
-    if(newTask.trim() !== ""){
-        tasks.push(newTask);
-        newTaskInput.value = "";
-        saveTasksToLocalStorage();
-        displayTasks();
-    } else {
-        alert("Please enter a task");
+    if (newTask.trim() !== ""){
+       tasks.push(newTask);
+       newTaskInput.value = "";
+       saveTaskToLocalStorage();
+       displayTasks();
+    } else{
+        alert('Please enter a task');
     }
-} 
+}
 
-const addTaskButton = document.querySelector("#addTaskBtn");
-addTaskButton.addEventListener('click', addTask);
-
-const loadTasksFromLocalStorage = () => {
-    const storedTasks = localStorage.getItem("tasks");
-
-    if(storedTasks){
-        tasks = JSON.parse(storedTasks);
+// ..................................................Edit Task......................................
+const editTask = (index) => {
+    const updatedTask = prompt("Update your task", tasks[index]);
+    if(updatedTask && updatedTask.trim() !== ""){
+        tasks[index] = updatedTask;
         displayTasks();
     }
 }
 
-loadTasksFromLocalStorage();
-// displayTasks(); 
+// ..................................................Delete Task......................................
+const deleteTask = (index) => {
+    if(confirm('Are you sure you want to delete this task?')){
+        tasks.splice(index, 1);
+        saveTaskToLocalStorage();
+        displayTasks();  
+    } 
+}
+
+// ..................................................Load Tasks from Local Storage......................................
+const loadTasksFromStorage = () =>{
+    const taskStored = localStorage.getItem('tasks');
+    if (taskStored) {
+        tasks = JSON.parse(taskStored);
+        displayTasks();
+    }
+}
+
+const addTaskButton = document.querySelector('#addTaskBtn');
+addTaskButton.addEventListener("click", addTask);
+
+loadTasksFromStorage();
+// displayTasks();
