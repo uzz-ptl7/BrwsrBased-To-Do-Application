a6bcf4c6d3905f8470475c428bb82cf84fd92485
uzz-ptl7 <uzair07patel@gmail.com>
2024-10-13 19:25:54 +0200

    Add Completed Tasks functionality button

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 // ....................................................Array of Tasks............................................
 let tasks = ["Buy milk","Clean the room","Go to the gym"];
+let completedTasks = [];
 
 // ........................Display Tasks, Create Tags and Buttons for Updating and Deleting Tasks..............................
 const displayTasks = () => {
     let taskDisplay = document.querySelector('#taskDisplay');
     taskDisplay.innerHTML = '';
 
+    // ..........................................Create Task Container.............................................
     tasks.forEach((task, index) => {
     const taskItem = document.createElement('li');
     taskItem.classList.add('bg-black', 'text-gray-200', 'p-2', 'rounded-lg','flex','justify-between', 'w-[60%]', 'm-auto', 'my-2');
     
+    // ..................................................Create Task Title Container......................................
     const taskText = document.createElement('span');
-
     taskText.textContent = task;
     taskItem.appendChild(taskText);
 
+    // .............................................Create Task Links Container................................
     const taskLinks = document.createElement('div');
     taskLinks.classList.add('task-links');
+
+    // ..................................................Mark as Done Button.....................................
+    const doneButton = document.createElement('a');
+    doneButton.href = '#';
+    doneButton.textContent = '✔️';
+    doneButton.classList.add('mr-4');
+    doneButton.addEventListener('click', () => markAsDone(index)); 
+    taskLinks.appendChild(doneButton); 
 
     // ..................................................Task Update Button......................................
     const updateButton = document.createElement('a');
@@ -32,24 +43,20 @@ const displayTasks = () => {
     deleteButton.textContent = '🗑️';
     // deleteButton.classList.add('text-black');
     deleteButton.addEventListener('click', () => deleteTask(index));
-    taskLinks.appendChild(deleteButton);
-    
+    taskLinks.appendChild(deleteButton); 
  
     taskItem.appendChild(taskLinks);
-
-    // taskItem.textContent = task;
     taskDisplay.appendChild(taskItem);
     })
 }
 
-//...............................Functions..........................................  
-
 // ..................................................Save Task to Local Storage......................................
 const saveTaskToLocalStorage = () => {
     localStorage.setItem('tasks', JSON.stringify(tasks))
+    localStorage.setItem('completedTasks', JSON.stringify(completedTasks))
 }
 
-// ..................................................Add Task to the Array......................................
+// ..................................................Add New Task to the Array......................................
 const addTask = () => {
     const newTaskInput = document.querySelector('#newTask');
     const newTask = newTaskInput.value;
@@ -82,17 +89,34 @@ const deleteTask = (index) => {
     } 
 }
 
+// ...................................................Mark as Done Button Functionality.................................
+const markAsDone = (index) => {
+    const task = tasks.splice(index, 1)[0];
+    completedTasks.push(task);
+    saveTaskToLocalStorage();
+    displayTasks();
+}
+
+
 // ..................................................Load Tasks from Local Storage......................................
 const loadTasksFromStorage = () =>{
-    const taskStored = localStorage.getItem('tasks');
-    if (taskStored) {
-        tasks = JSON.parse(taskStored);
+    const storedTasks = localStorage.getItem('tasks');
+    const storedCompletedTasks = localStorage.getItem('completedTasks');
+
+    if (storedTasks) {
+        tasks = JSON.parse(storedTasks);
         displayTasks();
+    }
+
+    if (storedCompletedTasks) {
+        completedTasks = JSON.parse(storedCompletedTasks);
     }
 }
 
+// ..................................................Add Task Button Functionality......................................
 const addTaskButton = document.querySelector('#addTaskBtn');
 addTaskButton.addEventListener("click", addTask);
 
+// ..................................................Calling Functions......................................
 loadTasksFromStorage();
 // displayTasks();
